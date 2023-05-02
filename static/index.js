9a848129117f3efab6ac8662f8d038f95d004c5e
10sasha <alex.pech841@gmail.com>
2023-05-03 00:05:50 +0300

    Data from dataset on the page

--- static/index.js
+++ static/index.js
@@ -1,4 +1,10 @@
 fetch('http://localhost:9874//flowers')
   .then(response => response.json())
-  .then(data => console.log(data))
-
+  .then(flowers => {
+    const flowersDiv = document.getElementById('flowers');
+    flowers.forEach(flower => {
+      const p = document.createElement('p');
+      p.innerText = `${flower.name}, ${flower.image_path}`;
+      flowersDiv.appendChild(p);
+    });
+  });
