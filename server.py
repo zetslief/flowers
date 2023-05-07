c07dad90d2c7884b326e7e7b565c79acf017ba52
Konstantin Konstantin <zetslief@protonmail.com>
2023-05-07 21:44:15 +0200

    Instance config implementation. Only PORT setting is used so far

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, jsonify
 import csv
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+CONFIG_FILE_NAME = 'config.py'
+app.config.from_pyfile(CONFIG_FILE_NAME)
 
 @app.route("/")
 def index():
@@ -17,4 +20,5 @@ def get_flowers():
     return jsonify(flowers)
 
 if __name__ == '__main__':
+    port = app.config['PORT'] if 'PORT' in app.config else 5000
     app.run(host='localhost', port=9874)
