de5b9d8f1c3ee1ff5a94c506343663754cf19ea1
Konstantin Konstantin <zetslief@protonmail.com>
2023-05-08 13:31:59 +0200

    Add card view to the `index.html`. Add button handling to the `index.js` file.

--- static/index.js
+++ static/index.js
@@ -1,22 +1,58 @@
-fetch('http://localhost:9874//flowers')
-  .then(response => response.json())
-  .then(flowers => {
+const FLOWERS_URL = document.location.origin + '/flowers';
+const CARD_BUY_URL = document.location.origin + '/card/buy';
+
+console.log(FLOWERS_URL);
+console.log(CARD_BUY_URL);
+
+mainView();
+cardView();
+
+function mainView() {
     const flowersDiv = document.getElementById('flowers');
-    flowers.forEach(flower => {
-      const p = document.createElement('p');
-      p.innerText = `${flower.name}, ${flower.image_path}`;
-      flowersDiv.appendChild(p);
-    });
+    const card = document.getElementById('card');
 
     const homeButton = document.getElementById('homeButton');
     const cardButton = document.getElementById('cardButton');
 
     homeButton.addEventListener('click', function() {
       flowersDiv.style.display = 'block';
+      card.setAttribute('hidden', 'hidden');
+      fetch(FLOWERS_URL)
+        .then(response => response.json())
+        .then(flowers => {
+            flowers.forEach(flower => {
+              const p = document.createElement('p');
+              p.innerText = `${flower.name}, ${flower.image_path}`;
+              flowersDiv.appendChild(p);
+            });
+          });
     });
 
     cardButton.addEventListener('click', function() {
       flowersDiv.style.display = 'none';
-      alert('Card view is not implemented yet!');
+      card.removeAttribute('hidden');
     });
-  });
+}
+
+
+function cardView() {
+  console.log("Card view initialization");
+  const form = document.getElementById('cardForm');
+  const firstName = document.getElementById('firstName'); 
+  const lastName = document.getElementById('lastName'); 
+  const email = document.getElementById('email'); 
+  const phone = document.getElementById('phone'); 
+  const address = document.getElementById('address'); 
+  const buyCard = document.getElementById('buyCard'); 
+  form.onsubmit = function(_) {
+    const data = {
+      firstName: form.elements.firstName.value,
+      lastName: form.elements.lastName.value,
+      email: form.elements.email.value,
+      phone: form.elements.phone.value,
+      address: form.elements.address.value,
+    };
+    console.log(JSON.stringify(data));
+    return false;
+  };
+}
